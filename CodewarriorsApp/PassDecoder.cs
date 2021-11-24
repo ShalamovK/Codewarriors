f5ca29f7cdfc1a3e1835ee8d2d528987e30da35e
Kirill <shalam.kirill@gmail.com>
2021-11-24 14:48:15 +0700

    init

---
+++ CodewarriorsApp/PassDecoder.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodewarriorsApp {
+    public static class TestClass {
+        public static bool Narcissistic(int value) {
+            string stringValue = value.ToString();
+            int length = stringValue.Length;
+            IEnumerable<int> digits = stringValue.ToCharArray().Select(x => Convert.ToInt32(x.ToString()));
+
+            int totalSum = 0;
+
+            foreach (int digit in digits) {
+                int poweredVal = (int)Math.Pow(digit, length);
+                totalSum += poweredVal;
+            }
+
+            return value == totalSum;
+        }
+
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems) {
+            foreach (object obj in listOfItems) {
+                if (obj is int) {
+                    yield return (int)obj;
+                }
+            }
+        }
+
+        public static int DescendingOrder(int num) {
+            string str = num.ToString();
+            string numbersOrdered = String.Join("", str.OrderByDescending(x => x));
+
+            if (Int32.TryParse(numbersOrdered, out int number)) {
+                return number;
+            } else {
+                return Int32.MaxValue;
+            };
+        }
+
+        public static int find_it(int[] seq) {
+            Dictionary<int, int> counterDict = new Dictionary<int, int>();
+
+            foreach (int number in seq) {
+                if (counterDict.ContainsKey(number)) {
+                    counterDict[number]++;
+                } else {
+                    counterDict.Add(number, 1);
+                }
+            }
+
+            return counterDict.First(x => x.Value % 2 != 0).Key;
+        }
+
+        public static String Accum(string s) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < s.Length; i++) {
+                string letter = s[i].ToString();
+
+                if (i > 0) {
+                    sb.Append("-");
+
+                    for (int k = 0; k < i; k++) {
+                        if (k == 0) {
+                            sb.Append(letter.ToUpper());
+                        } else {
+                            sb.Append(letter.ToLower());
+                        }
+                    }
+                } else {
+                    sb.Append(letter.ToUpper());
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static String Accum1(string s) {
+            return String.Join("-", s.Select((x, i) => $"{char.ToUpper(x)}{new string(char.ToLower(x), i)}"));
+        }
+    }
+}
---
+++ CodewarriorsApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodewarriorsApp {
+    class Program {
+        static void Main(string[] args) {
+            //bool isNarc = TestClass.Narcissistic(371);
+
+            //int result = TestClass.DescendingOrder(145263);
+            //string result = TestClass.Accum1("HbideVbxncC");
+
+            //Console.WriteLine(result);
+            //Console.ReadKey();
+
+            string sizeType;
+
+            Regex[] infantRegExps = new Regex[] {
+                new Regex(@"^[0-9]+[M,T]"),
+                new Regex(@"^[NB]")
+            };
+            Regex shorts = new Regex(@"^[W]??[0-9]{2}\Z");
+            Regex junior = new Regex(@"^[0-9]{1}\Z");
+            Regex pants = new Regex(@"^[W]??[0-9]{4}\Z");
+            Regex plusSize = new Regex(@"^[W]??[0-9]{4}\Z");
+            Regex[] regularSizes = new Regex[] {
+                new Regex(@"^[1-7]??[A-Z]+?[R,T,L,P]??\Z")
+            };
+            Regex youthSizes = new Regex(@"^[Y]+[1-7]??[A-Z]+?");
+
+            while (true) {
+                string sizeInput = Console.ReadLine();
+                string formattedSize = sizeInput.Replace(" ", "").ToUpper();
+
+                if (infantRegExps.Any(x => x.IsMatch(formattedSize))) {
+                    sizeType = "Infant";
+                } else if (shorts.IsMatch(formattedSize)) {
+                    sizeType = "Short";
+                } else if (pants.IsMatch(formattedSize)) {
+                    sizeType = "Pants";
+                } else if (junior.IsMatch(formattedSize)) {
+                    sizeType = "Junior";
+                } else if (youthSizes.IsMatch(formattedSize)) {
+                    sizeType = "Youth";
+                } else if (regularSizes.Any(x => x.IsMatch(formattedSize))) {
+                    sizeType = "Regular";
+                } else {
+                    sizeType = "Unknown";
+                }
+
+                Console.WriteLine(sizeType);
+            }
+        }
+    }
+}
